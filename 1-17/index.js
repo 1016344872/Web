97669068d9b2e7eeeb976f53d758e6a0885dbda4
koujio <1016344872@users.noreply.github.com>
2019-03-05 16:51:31 +0800

    3-5

--- 1-17/index.js
+++ 1-17/index.js
@@ -4,6 +4,10 @@ var moreCall = document.getElementById('morecontact');
 var clickmore = document.getElementById('clickmore');
 var moreDown2 = document.getElementById('moreDown2');
 
+
+window.onload=function(){
+	var delay = setInterval(countDown,1000);
+}
 //触发返回顶部的火箭动画
 rocket.onmouseover = function(){
 	rocket.classList.add('fly');
@@ -11,6 +15,27 @@ rocket.onmouseover = function(){
 rocket.onmouseout = function(){
 	rocket.classList.remove('fly');
 }
+//浏览器兼容性
+document.onscroll = function(){
+	var scroTop = window.scrollTop;
+	var scroLeft = window.scrollLeft;
+	if (typeof scroTop != "number") {
+		scroLeft = document.documentElement.scrollLeft;
+		scroTop = document.documentElement.scrollTop;
+	}else {
+		scroLeft = document.body.scrollLeft;
+		scroTop = document.body.scrollTop;
+	}
+console.log(scroTop);
+	if (scroTop>400) {
+		rocket.style.display = "block";
+	}else {
+		rocket.style.display = "none";
+	}
+
+}
+
+
 //触发联系栏的下拉内容
 /*function playanima(tar,cla){
 	if (tar.onmouseover) {
@@ -40,21 +65,19 @@ moreDown2.onmouseover = function(){
 moreDown2.onmouseout = function(){
 	moreDown2.style.display = 'none';
 }
-/*
-function delayPlay(){
-	setTimeout("clearPlay()",5000);
+//countDown倒计时
+function countDown(){
+	var date1 = new Date(2019,2,5,15,53,0,0,0); 
+	var date2 = new Date();
+	var nowDate = (date2 - date1);
+	var nowSeconds = Math.round(nowDate/1000)
+	var nowMinutes = Math.round(nowSeconds/60);
+	var nowHours = Math.round(nowMinutes/60);
+	var nowDay = Math.round(nowHours/24);
+	document.getElementById('nowDay').innerHTML = nowDay;
+	document.getElementById('nowHours').innerHTML = nowHours;
+	document.getElementById('nowMinutes').innerHTML = nowMinutes;
+	document.getElementById('nowSeconds').innerHTML = nowSeconds;
+
 }
-function clearPlay(){
-	moreCall.style.display = "none";
-}
-*/
-//浏览器兼容性
-var scroTop = window.scrollTop;
-var scroLeft = window.scrollLeft;
-if (typeof pagWidth != "number") {
-	scroLeft = document.documentElement.scrollLeft;
-	scroTop = document.documentElement.scrollTop;
-}else {
-	scroLeft = document.body.scrollLeft;
-	scroTop = document.body.scrollTop;
-}
+
