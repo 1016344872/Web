3ddff409fce662c58a41569aaa638f9f307c7d00
koujio <1016344872@users.noreply.github.com>
2019-03-05 09:44:58 +0800

    3-5

--- 1-17/index.js
+++ 1-17/index.js
@@ -1,6 +1,8 @@
 var rocket = document.getElementById('rocket');
 var moreDown = document.getElementById('mo');
 var moreCall = document.getElementById('morecontact');
+var clickmore = document.getElementById('clickmore');
+var moreDown2 = document.getElementById('moreDown2');
 
 //触发返回顶部的火箭动画
 rocket.onmouseover = function(){
@@ -10,18 +12,33 @@ rocket.onmouseout = function(){
 	rocket.classList.remove('fly');
 }
 //触发联系栏的下拉内容
+/*function playanima(tar,cla){
+	if (tar.onmouseover) {
+		cla.style.display = 'block';
+	}else if (tar.onmouseout) {
+		cla.style.display = 'none';
+	}
+}*/
 moreDown.onmouseover = function(){
 	moreCall.style.display = "block";
 }
 moreCall.onmouseover = function(){
 	moreCall.style.display = "block";
-	
 }
 moreDown.onmouseout = function(){
 	moreCall.style.display = "none";
 }
 moreCall.onmouseout = function(){
 	moreCall.style.display = "none";
+}
+clickmore.onmousedown = function(){
+	moreDown2.style.display = 'block';
+}
+moreDown2.onmouseover = function(){
+	moreDown2.style.display = 'block';
+}
+moreDown2.onmouseout = function(){
+	moreDown2.style.display = 'none';
 }
 /*
 function delayPlay(){
